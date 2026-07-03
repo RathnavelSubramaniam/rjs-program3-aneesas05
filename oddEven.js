0b30ece8c23177feb7a4ca520a020d82ca18e175
aneesas05 <uchihamadara0087@gmail.com>
2026-07-03 11:53:02 +0530

    Update oddEven.js

--- oddEven.js
+++ oddEven.js
@@ -5,9 +5,8 @@
 // Example:
 // checkOddEven(8) ? "Even"
 // checkOddEven(5) ? "Odd"
-
 const checkOddEven = (num) => {
   return( num % 2 === 0)?"even":"odd";
 };
-let number = 5;
+let number = 7;
 console.log(number + " is " + checkOddEven(number));
